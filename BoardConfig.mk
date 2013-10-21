32be6e91e3afa7228e1ddea1c364f9dac674588d
Seth Shelnutt <shelnutt2@gmail.com>
2013-10-21 18:24:50 -0400

    Rebase to be against gproj-common
    Change-Id: I3ba730dac69c28487d3c998198a6311ccf876eae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,13 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-BOARD_KERNEL_CMDLINE := vmalloc=600M console=null lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=e973
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
-# Try to build the kernel
-TARGET_KERNEL_SOURCE := kernel/lge/gproj
+BOARD_KERNEL_CMDLINE := vmalloc=600M console=null lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=e973
 TARGET_KERNEL_CONFIG := cyanogenmod_e970_defconfig
 
 # Recovery
