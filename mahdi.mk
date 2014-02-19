f0e5daed7bf966e38acd3bbc2606ebda5e47b7f5
Ron Asimi <ron.asimi@gmail.com>
2014-02-19 14:44:36 -0500

    initial commit for mahdi

--- mahdi.mk
+++ mahdi.mk
@@ -9,19 +9,19 @@ TARGET_SCREEN_WIDTH := 768
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mahdi/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mahdi/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e973/full_e973.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e973
-PRODUCT_NAME := cm_e973
+PRODUCT_NAME := mahdi_e973
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-E973
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=google/occam/GEEB_CA:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="gee-user 4.2.2 JDQ39 573038 release-keys"
+#PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=google/occam/GEEB_CA:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="gee-user 4.2.2 JDQ39 573038 release-keys"
